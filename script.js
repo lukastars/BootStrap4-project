aa5655aab57d54e7598d3890667fa92ca61e868c
lukastars <bonger2014@gmail.com>
2019-09-12 01:30:56 +0200

    beautify

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("nav-bar").style.top = "0";
@@ -11,10 +11,10 @@ window.onscroll = function() {
 }
 
 
-$( document ).ready(function() {
-    $('#headerVideoLink').magnificPopup({
-     type:'inline',
-     midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-   });
-     
-   });
+$(document).ready(function () {
+  $('#headerVideoLink').magnificPopup({
+    type: 'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
+
+});
